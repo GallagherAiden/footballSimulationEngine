7efbcac376134d5e0f313cc74b7a2a847d927d64
Aiden Gallagher <windymiller.aiden@gmail.com>
2018-09-24 22:32:47 +0100

    ignore test files for linter

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
-const expect = require('chai').expect;
-const common = require('../lib/common')
-const validation = require('./validate_tests');
+/* eslint-disable */
+const expect = require('chai').expect
+const validation = require('./validate_tests')
 
-describe('testValidationOfInputData()', function () {
-  it('init game returns an object', async () => {
+describe('testValidationOfInputData()', function() {
+  it('init game returns an object', async() => {
     // 1. ARRANGE
-    let t1location = "./init_config/team1.json"
-    let t2location = "./init_config/team2.json"
-    let plocation = "./init_config/pitch.json"
+    let t1location = './init_config/team1.json'
+    let t2location = './init_config/team2.json'
+    let plocation = './init_config/pitch.json'
 
     // 2. ACT
     let initJSON = await validation.initGame(t1location, t2location, plocation)
@@ -15,9 +15,9 @@ describe('testValidationOfInputData()', function () {
     // 3. ASSERT
     expect(initJSON).to.be.an('object')
   })
-  it('playIteration returns an Object', async () => {
+  it('playIteration returns an Object', async() => {
     // 1. ARRANGE
-  let providedItJson = "./init_config/iteration.json"
+    let providedItJson = './init_config/iteration.json'
 
     // 2. ACT
     let outputIteration = await validation.playIteration(providedItJson)
@@ -25,9 +25,9 @@ describe('testValidationOfInputData()', function () {
     // 3. ASSERT
     expect(outputIteration).to.be.an('object')
   })
-  it('start second half returns an Object', async () => {
+  it('start second half returns an Object', async() => {
     // 1. ARRANGE
-   let providedItJson = "./init_config/iteration.json"
+    let providedItJson = './init_config/iteration.json'
 
     // 2. ACT
     let shJSON = await validation.setupSecondHalf(providedItJson)
@@ -35,4 +35,4 @@ describe('testValidationOfInputData()', function () {
     // 3. ASSERT
     expect(shJSON).to.be.an('object')
   })
-});
+})
--- test/validate_tests.js
+++ test/validate_tests.js
@@ -1,52 +1,53 @@
+/* eslint-disable */
 const common = require('../lib/common')
 const engine = require('../engine')
 
-async function initGame(t1, t2, p){
+async function initGame(t1, t2, p) {
   let team1 = await common.readFile(t1)
-  .catch(function(err){
+    .catch(function(err) {
       throw err.stack
-  })
+    })
   let team2 = await common.readFile(t2)
-  .catch(function(err){
-    throw err.stack
-})
+    .catch(function(err) {
+      throw err.stack
+    })
   let pitch = await common.readFile(p)
-  .catch(function(err){
-    throw err.stack
-})
+    .catch(function(err) {
+      throw err.stack
+    })
   let matchSetup = engine.initiateGame(team1, team2, pitch)
-  .catch(function(err){
-    throw err.stack
-})
+    .catch(function(err) {
+      throw err.stack
+    })
   return matchSetup
 }
 
-async function playIteration(inputIteration){
+async function playIteration(inputIteration) {
   let inputJson = await common.readFile(inputIteration)
-  .catch(function(err){
-    throw err.stack
-  })
+    .catch(function(err) {
+      throw err.stack
+    })
   let outputIteration = await engine.playIteration(inputJson)
-  .catch(function (err) {
-		throw err.stack
-  })
+    .catch(function(err) {
+      throw err.stack
+    })
   return outputIteration
 }
 
-async function setupSecondHalf(inputIteration){
+async function setupSecondHalf(inputIteration) {
   let inputJson = await common.readFile(inputIteration)
-  .catch(function(err){
-    throw err.stack
-  })
+    .catch(function(err) {
+      throw err.stack
+    })
   let outputJSON = await engine.startSecondHalf(inputJson)
-  .catch(function (err) {
-		throw err.stack
-  })
+    .catch(function(err) {
+      throw err.stack
+    })
   return outputJSON
 }
 
 module.exports = {
-    initGame,
-    playIteration,
-    setupSecondHalf
-  }
+  initGame,
+  playIteration,
+  setupSecondHalf
+}
