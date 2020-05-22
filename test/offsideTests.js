988ecf8bc3b4d28cc127e25f54cb07700bd7e109
Aiden Gallagher <aiden.g@live.co.uk>
2020-05-23 00:38:40 +0100

    updated to offside rules

---
+++ test/offsideTests.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-unused-vars */
+/*eslint-disable max-len*/
+const mocha = require('mocha')
+const { expect } = require('chai')
+const pMovement = require('../lib/playerMovement')
+const common = require('../lib/common')
+
+function runTest() {
+  mocha.describe('checkOffside()', function () {
+    mocha.it('1 Bottom Player offside', async () => {
+      let matchDetails = await common.readFile('test/input/offsideTests/offsidePosition1.json')
+      let team = matchDetails.secondTeam
+      let opposition = matchDetails.kickOffTeam
+      pMovement.checkOffside(team, opposition, matchDetails)
+      expect(JSON.stringify(matchDetails).split(`"offside":true`).length - 1).to.eql(1)
+      expect(matchDetails.secondTeam.players[9].offside).to.eql(true)
+    })
+    mocha.it('1 Player top offside', async () => {
+      let matchDetails = await common.readFile('test/input/offsideTests/offsideTopPosition1.json')
+      let team = matchDetails.secondTeam
+      let opposition = matchDetails.kickOffTeam
+      pMovement.checkOffside(team, opposition, matchDetails)
+      expect(JSON.stringify(matchDetails).split(`"offside":true`).length - 1).to.eql(1)
+      expect(matchDetails.kickOffTeam.players[9].offside).to.eql(true)
+    })
+    mocha.it('2 Player offside', async () => {
+      let matchDetails = await common.readFile('test/input/offsideTests/offsidePosition2.json')
+      let team = matchDetails.secondTeam
+      let opposition = matchDetails.kickOffTeam
+      pMovement.checkOffside(team, opposition, matchDetails)
+      expect(JSON.stringify(matchDetails).split(`"offside":true`).length - 1).to.eql(2)
+      expect(matchDetails.secondTeam.players[9].offside).to.eql(true)
+      expect(matchDetails.secondTeam.players[10].offside).to.eql(true)
+    })
+    mocha.it('1 offside player, same team with ball', async () => {
+      let matchDetails = await common.readFile('test/input/offsideTests/onsidePosition1.json')
+      let team = matchDetails.secondTeam
+      let opposition = matchDetails.kickOffTeam
+      team.players[9].currentPOS[1] = 121
+      matchDetails.ball.position[1] = 121
+      team.players[10].currentPOS = [400, 57]
+      pMovement.checkOffside(team, opposition, matchDetails)
+      expect(JSON.stringify(matchDetails).split(`"offside":true`).length - 1).to.eql(1)
+      expect(matchDetails.secondTeam.players[9].offside).to.eql(false)
+      expect(matchDetails.secondTeam.players[10].offside).to.eql(true)
+    })
+    mocha.it('Goalkeeper has ball, no offside positions', async () => {
+      let matchDetails = await common.readFile('test/input/offsideTests/onsideBottomPosition2GK.json')
+      let team = matchDetails.secondTeam
+      let opposition = matchDetails.kickOffTeam
+      pMovement.checkOffside(team, opposition, matchDetails)
+      expect(JSON.stringify(matchDetails).split(`"offside":true`).length - 1).to.eql(0)
+      expect(matchDetails.secondTeam.players[9].offside).to.eql(false)
+      expect(matchDetails.secondTeam.players[10].offside).to.eql(false)
+    })
+    mocha.it('Ball not with team', async () => {
+      let matchDetails = await common.readFile('test/input/offsideTests/ballNotWithTeam.json')
+      let team = matchDetails.secondTeam
+      let opposition = matchDetails.kickOffTeam
+      pMovement.checkOffside(team, opposition, matchDetails)
+      expect(JSON.stringify(matchDetails).split(`"offside":true`).length - 1).to.eql(0)
+      expect(matchDetails.secondTeam.players[9].offside).to.eql(false)
+      expect(matchDetails.secondTeam.players[10].offside).to.eql(false)
+    })
+  })
+}
+
+module.exports = {
+  runTest
+}
